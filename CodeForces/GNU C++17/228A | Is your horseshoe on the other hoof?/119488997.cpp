9b92c85c21c86ee53e0ba8c7a8739104d017ba0f
OMR125 <85144803+OMR125@users.noreply.github.com>
2023-05-01 03:37:30 +0200

    Committing files

---
+++ CodeForces/GNU C++17/228A | Is your horseshoe on the other hoof?/119488997.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+ 
+using namespace std;
+ 
+int main()
+{
+   int a[4],cnt=0;
+   for(int i=0;i<4;i++){
+    cin >> a[i];
+   }
+   sort(a,a+4);
+   for(int i=0;i<4;i++){
+    if(a[i]==a[i+1]){
+        cnt++;
+    }
+   }
+   cout << cnt;
+    return 0;
+}

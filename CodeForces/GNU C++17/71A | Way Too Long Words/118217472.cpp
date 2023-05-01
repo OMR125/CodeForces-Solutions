72f0be27e79f4ffd6abe5af260ece06d789533ad
OMR125 <85144803+OMR125@users.noreply.github.com>
2023-05-01 03:39:07 +0200

    Committing files

---
+++ CodeForces/GNU C++17/71A | Way Too Long Words/118217472.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+int n;
+string w;
+cin >> n;
+for(int i = 0 ; i<n ; i++){
+    cin >> w ;
+    if(w.size() >10){
+        cout << w[0] << w.size()-2 << w[w.size()-1] << endl;
+    }
+    else{cout << w << endl;}
+}
+    return 0;
+}

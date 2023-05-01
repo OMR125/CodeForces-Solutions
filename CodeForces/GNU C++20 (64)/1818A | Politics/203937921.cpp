086dce609e4b8b990ea7a72c40652a8d79b0099c
OMR125 <85144803+OMR125@users.noreply.github.com>
2023-05-01 03:34:02 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1818A | Politics/203937921.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+struct default_map_val{long long i = -1;};
+typedef long long ll;
+using namespace std;
+using namespace __gnu_pbds;
+#define OMR125 cin.tie(0), cout.tie(0), cin.sync_with_stdio(0), cout.sync_with_stdio(0);
+#define pp pair<ll,ll>
+#define out(flag) cout << (flag ? "YES" : "NO") << endl;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define all(v) v.begin(),v.end()
+#define prefix(arr,n) for(ll i=1;i<=n;i++){arr[i]+=arr[i-1];}
+#define partial(arr,l,r,k) arr[l] += k; arr[r + 1] -= k;
+const int mxN = 2e5;
+bool sortbysec(pp a, pp b){return a.second<b.second;}
+ 
+ 
+ 
+int main() {
+    OMR125
+    int t,n,a;cin >> t;
+    while(t--){
+        cin >> n >> a;
+        string s;
+        cin >> s;
+        int ans=1;
+        for(int i=0;i<n-1;i++){
+            string temp;
+            cin >> temp;
+            if(temp==s)ans++;
+        }
+        cout << ans << "\n";
+    }
+ 
+}
+ 

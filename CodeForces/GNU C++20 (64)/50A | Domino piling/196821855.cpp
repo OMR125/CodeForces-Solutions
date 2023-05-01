e214c6171117213b86beede85d922f96fa57aaf1
OMR125 <85144803+OMR125@users.noreply.github.com>
2023-05-01 03:36:34 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/50A | Domino piling/196821855.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <map>
+#include <iomanip>
+ 
+ 
+using namespace std;
+ 
+ 
+int main()
+{   cin.tie(0), cout.tie(0), cin.sync_with_stdio(0), cout.sync_with_stdio(0);
+    int a,b;
+    cin >> a >> b;
+    cout << (a*b)/2;
+ 
+ 
+    return 0;
+}
